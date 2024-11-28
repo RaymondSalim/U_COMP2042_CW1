3936de951a34b0231e8e14b9bf07346b68e05674
Raymond Salim <rsalim@domaintools.com>
2024-11-28 22:39:39 +0800

    refactor: changed image file name

--- src/main/java/com/example/demo/view/screens/levels/LevelFour.java
+++ src/main/java/com/example/demo/view/screens/levels/LevelFour.java
@@ -5,8 +5,8 @@ import javafx.scene.Group;
 import javafx.scene.image.Image;
 
 public class LevelFour extends LevelView {
-    private static final String BACKGROUND_IMAGE_NAME_1 = "/com/example/demo/images/level/bg2.png";
-    private static final String BACKGROUND_IMAGE_NAME_2 = "/com/example/demo/images/level/bg2r.png";
+    private static final String BACKGROUND_IMAGE_NAME_1 = "/com/example/demo/images/level/bg4.png";
+    private static final String BACKGROUND_IMAGE_NAME_2 = "/com/example/demo/images/level/bg4r.png";
 
     public LevelFour(Group root, int heartsToDisplay) {
         super(root, heartsToDisplay);
