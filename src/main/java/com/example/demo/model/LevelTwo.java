1b2cc0d40a040b61b85a072070aed13e0e5db1aa
Raymond Salim <rsalim@domaintools.com>
2024-11-29 12:32:47 +0800

    feat: add LevelTwo

---
+++ src/main/java/com/example/demo/model/LevelTwo.java
@@ -0,0 +1,39 @@
+package com.example.demo.model;
+
+import com.example.demo.enums.LevelType;
+import com.example.demo.model.base.LevelParent;
+import com.example.demo.view.base.LevelView;
+
+public class LevelTwo extends LevelParent {
+    private static final int PLAYER_INITIAL_HEALTH = 5;
+
+    public LevelTwo() {
+        super(PLAYER_INITIAL_HEALTH);
+
+        super.NEXT_LEVEL = LevelType.LEVEL_THREE;
+        super.MAX_ENEMY_SPAWN = 20;
+        super.MAX_ENEMIES_AT_A_TIME = 4;
+    }
+
+    @Override
+    protected int calculateStars(int score) {
+        if (score < 1500) {
+            return 0;
+        }
+
+        if (score <= 1700) {
+            return 1;
+        }
+
+        if (score <= 1900) {
+            return 2;
+        }
+
+        return 3;
+    }
+
+    @Override
+    protected LevelView instantiateLevelView() {
+        return new com.example.demo.view.screens.levels.LevelTwo(getRoot(), PLAYER_INITIAL_HEALTH);
+    }
+}
---
+++ src/main/java/com/example/demo/view/screens/levels/LevelTwo.java
@@ -0,0 +1,19 @@
+package com.example.demo.view.screens.levels;
+
+import com.example.demo.view.base.LevelView;
+import javafx.scene.Group;
+import javafx.scene.image.Image;
+
+public class LevelTwo extends LevelView {
+    private static final String BACKGROUND_IMAGE_NAME_1 = "/com/example/demo/images/level/bg2.png";
+    private static final String BACKGROUND_IMAGE_NAME_2 = "/com/example/demo/images/level/bg2r.png";
+
+    public LevelTwo(Group root, int heartsToDisplay) {
+        super(root, heartsToDisplay);
+
+        Image backgroundImage = new Image(getClass().getResource(BACKGROUND_IMAGE_NAME_1).toExternalForm());
+        Image backgroundImage2 = new Image(getClass().getResource(BACKGROUND_IMAGE_NAME_2).toExternalForm());
+
+        this.initializeBackground(backgroundImage, backgroundImage2);
+    }
+}
